e65f4add441caa1102e9c1ba67279d2337d5dc2b
Igor Leonor Macedo <igl_macedo@hotmail.com>
2025-06-28 10:00:50 -0300

    V.1.3.2 - Implementado o código da tela home, melhorando a responsividade da tela.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './page/login/login';
+import {HomeComponent} from './page/home/home';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent}
 ];
