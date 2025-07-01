f531a7687afd479b08b3e732cbb5d61635fe5f25
Iot-Health-Br <igl_macedo@hotmail.com>
2025-06-30 21:18:14 -0300

    V.1.3.4 - Incrementado metodo de validação de login com JWT.

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,5 +1,5 @@
   import {Injectable} from '@angular/core';
-  import {catchError, Observable, tap, throwError} from 'rxjs';
+  import {BehaviorSubject, catchError, Observable, tap, throwError} from 'rxjs';
   import {HttpClient} from "@angular/common/http";
   import {Router} from '@angular/router';
 
@@ -7,20 +7,30 @@
       providedIn: 'root'
     })
     export class LoginService{
-      private urlLogin = 'http://localhost:8080/authenticate';
+      private apiUrl = 'http://localhost:8080'; // URL do seu Spring Boot
+      private tokenSubject = new BehaviorSubject<string | null>(null);
+
 
       constructor(private router: Router, private http: HttpClient) {
+
       }
 
-      login(cpf:string, password: string):Observable<Login>{
-        return this.http.post<Login>(this.urlLogin, { cpf, password})
+      login(cpf:string, password: string):Observable<string>{
+        const credentials = btoa(`${cpf}:${password}`); // Codifica para Basic Auth
+        const headers = {
+          'Authorization': `Basic ${credentials}`,
+          'Content-Type': 'application/json'
+        };
+
+        return this.http.post(`${this.apiUrl}/authenticateUser`, {}, { headers, responseType: 'text' })
           .pipe(
             tap(response => {
               if (response) {
-
+                localStorage.setItem('token', response);
+                this.tokenSubject.next(response);
               }
               else {
-                console.log('Resposta de erro:', response);
+                console.log('Erro:', response);
               }
             }),
             catchError(error => {
@@ -31,5 +41,27 @@
               return throwError(() => error);
             })
           );
+      }
+      logout(): void {
+        localStorage.removeItem('token');
+        this.tokenSubject.next(null);
+      }
+
+      getToken(): string | null {
+        return this.tokenSubject.value;
+      }
+
+      isAuthenticated(): boolean {
+        return !!this.getToken();
+      }
+
+      // Decodificar JWT para pegar dados do usuário
+      getUserData(): any {
+        const token = this.getToken();
+        if (token) {
+          const payload = JSON.parse(atob(token.split('.')[1]));
+          return payload;
+        }
+        return null;
       }
     }
