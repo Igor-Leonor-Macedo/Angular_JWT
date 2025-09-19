51f8c61e4b2384aea6fb98e3dd56644a9844e879
Igor <iglmacedo@gmail.com>
2025-09-19 12:55:21 -0300

    V.1.4.6 - Implementado o component de spinner na tela Login.

--- src/app/component/loading-spinner/loading-spinner.ts
+++ src/app/component/loading-spinner/loading-spinner.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-loading-spinner',
-  imports: [MatProgressSpinnerModule],
+  imports: [
+    MatProgressSpinnerModule,
+    CommonModule
+  ],
   templateUrl: './loading-spinner.html',
   styleUrl: './loading-spinner.css'
 })
 export class LoadingSpinner {
-
+  @Input() loading = false;
+  @Input() delay = 0; // tempo de delay em milissegundos
 }
